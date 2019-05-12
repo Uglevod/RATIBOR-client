ad6f658281356ec6ed582eb8c738f4743423c89f
Ucl.org.ru <vauglev@gmail.com>
2019-05-12 15:21:34 +0700

    z

---
+++ index.php
@@ -0,0 +1,110 @@
+<?php 
+$stage = intval(file_get_contents('tb.txt'));
+
+
+$date=strval(date("d"));
+//echo $stage;
+
+
+$string = file_get_contents("./tasks/".$date.".json");
+$jsn = json_decode($string, true);
+
+$do="";
+switch ($stage){
+        
+    case 1 : {$do="st"; break;}
+    case 2 : {$do="st"; break;}
+    case 3 : {$do="relax"; break;}
+    case 4 : {$do="nd"; break;}
+    case 5 : {$do="nd"; break;}    
+    case 6 : {$do="relax"; break;}    
+    case 7 : {$do="td"; break;}    
+    case 8 : {$do="td"; break;}
+    case 9 : {$do="relax"; break;}    
+    default:  {
+        $do="relax"; break;
+    }
+
+}
+
+//echo $jsn[$do]["proj"];
+
+?>
+
+<!doctype html>
+<html lang="ru">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="<?php echo $jsn[$do]['fon'];?> ">
+
+    <title>Userv</title>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <nav class="navbar navbar-dark bg-dark">
+            PinoChette
+        </nav>
+        <div class="row">
+            <div class="col phol">
+
+            </div>
+        </div>
+
+        <div class="row align-items-center justify-content-center">
+            <div class="col-7 ">
+                <div class="card" style="width: 50rem;">
+                    <div class="card-body">
+                        <h5 class="card-title"> <?php echo $jsn[$do]['stage'];?> </h5>
+                        <h6 class="card-subtitle mb-2 text-muted">
+                            <?php echo $jsn[$do]['proj'];?>
+                        </h6>
+                        <p class="card-text">
+
+                            <ul>
+                                <?php 
+                                $arr=$jsn[$do]['tasks'];
+                                foreach ($arr as $a)
+                                {
+                                echo "<li>".$a."</li>";
+                                }
+
+            ?>
+                            </ul>
+
+
+                        </p>
+                        <a href="<?php echo $jsn[$do]['url'];?>" class="card-link">
+                            Подробнее
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+
+
+
+    </div>
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="jquery.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="bootstrap.js"></script>
+</body>
+
+</html>
--- startmsh.py
+++ startmsh.py
@@ -5,34 +5,46 @@ import sys
 import curses
 
 
-def sl(t,s):
+def sl(t,s,b):
 	stdscr = curses.initscr()
 	for i in range(t):
-		stdscr.addstr(0, 0,"  "+str(s)+" stage min left"+str(t-i))
+		stdscr.addstr(0, 0,"  "+str(s)+" stage min left "+str(t-i))
 		stdscr.refresh()
+		os.system('echo '+b+' > tb.txt')
 		time.sleep(60)
 
 print ("Start")
-os.system('timeout 3s timidity ./midi/trava_u_doma.mid')
-#os.system(time.clock())
-sl(40,"Work 1")
+# start 1 st time box
+os.system('timeout 3m timidity ./midi/trava_u_doma.mid')
+sl(40,"Work 1",		"1")
 	
-os.system('timeout 3m timidity ./midi/eto_vse.mid')
-sl(10,"End Work 1")
 os.system('timeout 3m timidity ./midi/lesnik.mid')
-sl(10,"Relax 1")
+sl(10,"End Work 1",	"2")
 
+os.system('timeout 3m timidity ./midi/eto_vse.mid')
+sl(10,"Relax 1",	"3")
+
+# start 2 st time box
 os.system('timeout 3m timidity ./midi/saundtrek.mid')
-sl(40,"Work 2")
-os.system('timeout 3m timidity ./midi/osen.mid')
-sl(10,"End Work 2")
+sl(40,"Work 2",		"4")
+
 os.system('timeout 3m timidity ./midi/lesnik.mid')
-sl(10,"Relax 2")
+sl(10,"End Work 2",	"5")
 
+os.system('timeout 3m timidity ./midi/osen.mid')
+sl(10,"Relax 2",	"6")
 
+# start 2 st time box
 os.system('timeout 3m timidity ./midi/main_theme.mid')
-sl(40,"Work 3")
-os.system('timeout 3m timidity ./midi/eto_vse.mid')
-sl(10,"end Work 3")
+sl(40,"Work 3",		"7")
+
+
 os.system('timeout 3m timidity ./midi/ya_svoboden.mid')
-sl(10,"relax 3")
+sl(10,"end Work 3",	"8")
+
+os.system('timeout 3m timidity ./midi/eto_vse.mid')
+sl(30,"relax 3",	"9")
+
+#big relax
+
+
